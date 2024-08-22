99f44cac4ae7f118b45f05a78b93da40066a8a9e
kerryl02 <sodakerrylgmail.com>
2024-08-22 23:04:31 +0200

    setup :egg:

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,8 +1,14 @@
+'use client';
 import React from 'react'
+interface ContainerProps {
+    children: React.ReactNode;
+}
 
-const Container = () => {
+const Container: React.FC<ContainerProps> = ({children}) => {
   return (
-    <div>Container</div>
+    <div className='max-w-[2520px mx-auto xl:px-20 md:px-10 sm:px-6 px-4'>
+        {children}
+    </div>
   )
 }
 
