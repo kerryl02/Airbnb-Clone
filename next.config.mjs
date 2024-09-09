e101553deb67db5354fcacbbf78e9f366535cefc
kerryl02 <sodakerrylgmail.com>
2024-09-09 10:34:40 +0200

    :egg:

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "avatar.githubusercontent.com",
+            "lh3.googleusercontent.com"
+        ]
+    }
+};
+
+export default nextConfig;
